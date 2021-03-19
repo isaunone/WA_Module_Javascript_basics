3b50619f6e235eb124072ed5e7637a7ff3f3f178
isaunone <valerievarias@gmail.com>
2021-03-19 12:56:24 +0100

    level 12 opdracht 1

---
+++ Level12_odpracht1/script.js
@@ -0,0 +1,22 @@
+console.log("Hello Winc Academy")
+
+let name = 'Valerie';
+let number = 4 + 4 ;
+let string = '4 + 4'; 
+let add = 10 + 1; 
+let subtraction = 10 - 4; 
+let multiplication = 10 * 2; 
+let divide = 10 / 5; 
+let modulus = 9 % 3; 
+let age = 28;
+let ageString = '28';
+console.log(name);
+console.log(number);
+console.log(string);
+console.log(add);
+console.log(subtraction);
+console.log(multiplication);
+console.log(divide);
+console.log(modulus);
+console.log(typeof(age));
+console.log(typeof(ageString));
