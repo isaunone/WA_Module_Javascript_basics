a746e2cb1af569cee82095eda165226ad26791bf
isaunone <valerievarias@gmail.com>
2021-04-16 11:07:05 +0200

    level12 opdracht2

---
+++ Level12_odpracht2/script.js
@@ -0,0 +1,10 @@
+console.log("Hello Winc Academy")
+
+// Dit is een grote som.
+let multiplication = 1230941 * 1823794;
+let division = 1637263 / 54;
+
+/*
+console.log(multiplication);
+console.log(division);
+*/
