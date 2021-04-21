5b76d290e30193f3ef7d7e9ffc52db74531b34f2
isaunone <valerievarias@gmail.com>
2021-04-21 16:16:27 +0200

    level13 opdracht 1

---
+++ Level13_opdrach1/script.js
@@ -0,0 +1,21 @@
+const age = 17;
+const isFemale = true;
+const driverStatus = 'bob';
+
+if (age >= 18) {
+    console.log('Come inside get some beer!');
+}else {
+    console.log('Younger than 18, please stay outside')
+}
+
+if (isFemale) {
+    console.log('A female');
+}else {
+    console.log('Not a female');
+}
+
+if (driverStatus == 'bob') {
+    console.log('You can drive');
+}else {
+    console.log('Your not allowed to drive');
+}
